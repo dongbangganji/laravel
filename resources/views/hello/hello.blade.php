dce2da257f92cbece9144b457b10eba68ab8102b
Carry <lsy7651@naver.com>
2017-03-24 18:10:24 +0900

    origin

---
+++ resources/views/hello/hello.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="utf-8">
+    <title>배달은 우리가 통이니까, 배달통</title>
+</head>
+<body>
+<h1>라라벨이란?</h1>
+<h2>라라벨은 가장 모던하고 세련된 php 프레임워크이며, 유연하고 세련된 기능을 제공합니다.</h2>
+<h3></h3>
+</body>
+</html>
