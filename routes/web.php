93a4b312bf039c260802a0ae9013f00f851918f8
Carry <lsy7651@naver.com>
2017-05-22 12:24:21 +0900

    revert test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -8,7 +7,7 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+|sddd
 */
 
 /**
@@ -65,6 +64,7 @@ Route::get('calc/{num}', function ($num) {
 Route::get('unless/{level}', function ($level) {
     return view('unless.unless')->with('level',$level);
 });
+
 
 /**
  * 반복문
